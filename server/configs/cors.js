e99b6992b6d6c248432d3dd036e18b746fea75e2
leecoders <als-1994@hanmail.net>
2019-11-16 21:33:48 +0900

    Feat: cors 설정 및 화이트리스트 옵션 추가
    ?: client와 server의 url이 다른 cors 이슈 해결,
    인증서(쿠키)를 fetch에 담기 위한 화이트리스트 옵션 추가 설정
    
    configs/cors.js로 모듈화

---
+++ server/configs/cors.js
@@ -0,0 +1,13 @@
+const whitelist = [process.env.EXPRESS_APP_FRONTEND_HOST];
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+};
+
+module.exports = corsOptions;
