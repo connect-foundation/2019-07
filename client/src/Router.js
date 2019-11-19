aa058113d31207f96d28a79af069dd6935a114c7
Changyeong <ocy1011@naver.com>
2019-11-20 00:05:55 +0900

    Refactor: MainPage 리팩토링
     ?: 모바일 가로화면에서 MainPage의 내용이 다 보이지 않았는데, 스크롤도 되지 않아서 style 수정.
    MainPage의 component들은 다른 곳에서 사용하지 않을 재사용성이 떨어지는 것들이라고 판단.
    따라서 component 파일을 제거하고 MainPage 안에서 생성하도록 변경.
    
    pages의 mainPage 디렉토리에 MainPage 하나만 존재하게 될텐데 디렉토리의 의미가 없는 것으로 판단.
    따라서 mainPage 디렉토리를 제거하고 pages 다음에 바로 MainPage.js가 오도록 위치 변경.

--- client/src/Router.js
+++ client/src/Router.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import MainPage from './pages/mainPage/MainPage';
+import MainPage from './pages/MainPage';
 import HostWaitingRoom from './pages/host/HostWaitingRoom';
 import PlayerWaitingRoom from './pages/player/PlayerWaitingRoom';
 
-export default function () {
+export default function() {
   return (
     <Router>
       <Switch>
