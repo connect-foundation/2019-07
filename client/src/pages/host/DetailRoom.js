c57c6c582b37fc0ce43779ea241a6fe20b6e31f3
2ssue <e2ssue@gmail.com>
2019-11-19 20:23:54 +0900

    Feat: 방의 이름을 수정하는 방식 변경
    ?: 모바일에서 사용성이 떨어져 click할 버튼을 생성
    모바일에서는 hover가 존재하지않고, doubleClick은 일반적이지 않은
    이벤트이기 때문에 방 이름 수정 이미지 버튼을 추가, 해당 버튼을 통해
    방의 이름을 변경할 수 있도록 변경함

--- client/src/pages/host/DetailRoom.js
+++ client/src/pages/host/DetailRoom.js
@@ -15,7 +15,7 @@ const RoomInfoWrapper = styled.div`
   transform: translateY(-50%);
   display: flex;
   align-items: center;
-  * {
+  span {
     color: ${colors.TEXT_WHITE};
     font-weight: bold;
     font-size: 2rem;
@@ -24,12 +24,16 @@ const RoomInfoWrapper = styled.div`
   }
 `;
 
-const RoomName = styled.button`
-  background-color: inherit;
-  border: none;
+const EditRoomNameImage = styled.img.attrs({
+  src:
+    'https://iconmonstr.com/wp-content/g/gd/makefg.php?i=../assets/preview/2012/png/iconmonstr-edit-4.png&r=255&g=255&b=255',
+})`
+  width: 2.5rem;
   cursor: pointer;
+  opacity: 0.6;
   &:hover {
     text-decoration: underline;
+    opacity: 1;
   }
   &:focus {
     outline: none;
@@ -50,9 +54,6 @@ const NavigationBar = styled.nav`
   box-shadow: 0 5px 5px -4px ${colors.TEXT_GRAY};
   justify-content: center;
   align-items: center;
-
-  button {
-  }
 `;
 
 const TabMenuButton = styled.div`
@@ -80,13 +81,13 @@ function DetailRoom() {
   const [isQuizMenuSelected, setQuizMenuState] = useState(true);
   const [isAnalysisMenuSelected, setAnalysisMenuState] = useState(false);
 
-  function changeRoomName(e) {
-    const currentRoomName = e.target.innerText;
+  function changeRoomName() {
     /**
      * 현재 방의 이름을 Modal input에 출력
      * 유저가 입력한 값을 setRoomName로 설정하고 fetch
      * 응답이 실패로 왔을 경우 상태를 이전으로 돌리고 유저에게 알림
      * */
+    const currentRoomName = roomName;
     setRoomName(currentRoomName);
   }
 
@@ -125,9 +126,8 @@ function DetailRoom() {
     <Background>
       <Header>
         <RoomInfoWrapper>
-          <RoomName title="이름 수정하기" onDoubleClick={changeRoomName}>
-            {roomName}
-          </RoomName>
+          <span>{roomName}</span>
+          <EditRoomNameImage title="이름 수정하기" onClick={changeRoomName} />
           <span>{roomNumber}</span>
         </RoomInfoWrapper>
         <ButtonContainer>
