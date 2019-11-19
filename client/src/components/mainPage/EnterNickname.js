4fba2e867dba42169aa0d6d02aba3ba5120ee5cf
leecoders <als-1994@hanmail.net>
2019-11-19 13:21:32 +0900

    Feat: client측 방 번호 서버 요청을 위한 스켈레톤 작성, 라우팅 처리
    ?: 서버 요청을 방 번호를 요청하여 결과를 응답 받고, 결과에 따른 분기 처리를 위한 스켈레톤 작성
    
    방 번호 input에 대해 엔터, 입장하기 버튼 클릭에 대해 move 함수 실행
    history를 통한 클라이언트 사이드 페이지 라우팅 처리

--- client/src/components/mainPage/EnterNickname.js
+++ client/src/components/mainPage/EnterNickname.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import * as styles from '../../styles/common';
 import { GreenButton } from '../common/Buttons';
@@ -14,29 +14,31 @@ const Input = styled.input`
   ${styles.InputStyle}
 `;
 
-function EnterNickname() {
-  const [name, setName] = useState('');
+function EnterNickname({ history }) {
+  const { roomNumber } = history.location.state;
+  const [nickname, setNickname] = useState('');
 
-  function handleInput(e) {
-    setName(e.target.value);
+  function handleInputChange(e) {
+    setNickname(e.target.value);
   }
 
   return (
     <>
-      <Input placeholder="닉네임" onChange={handleInput} />
-      <Link to={{
-        pathname: '/player',
-        state: {
-          nickname: name,
-        },
-      }}
-      >
-        <ButtonWrapper>
-          <GreenButton>닉네임 정하기</GreenButton>
-        </ButtonWrapper>
-      </Link>
+      <Input placeholder="닉네임" onChange={handleInputChange} />
+      <ButtonWrapper>
+        <GreenButton>닉네임 정하기</GreenButton>
+      </ButtonWrapper>
     </>
   );
 }
+
+EnterNickname.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+    location: PropTypes.shape({
+      state: PropTypes.shape({ roomNumber: PropTypes.string.isRequired }),
+    }),
+  }).isRequired,
+};
 
 export default EnterNickname;
