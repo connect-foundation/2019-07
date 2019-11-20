9aecb7146ab6cceb2ffc78177482ae17b7bfe006
2ssue <e2ssue@gmail.com>
2019-11-20 14:48:25 +0900

    Refactor: state를 가진 element 컴포넌트화
    ?: state를 가지는 경우 컴포넌트화 하기로 하였으므로 단위를 쪼갬
    방의 이름과, 방의 퀴즈 및 분석을 가져오는 부분은 같은 방의 정보
    이지만 이 둘이 같은 컴포넌트에 있으면 실질적인 영역이 다르고,
    컴포넌트의 단위가 너무 크다고 판단되어 단위를 두개로 나눔.

--- client/src/pages/host/HostDetailRoom.js
+++ client/src/pages/host/HostDetailRoom.js
@@ -1,44 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import * as colors from '../../constants/colors';
 import Header from '../../components/common/Header';
 import { YellowButton } from '../../components/common/Buttons';
+import TabContents from '../../components/detailRoom/TabContents';
+import RoomInformation from '../../components/detailRoom/RoomInformation';
 
 const Background = styled.div`
   height: 100%;
   background-color: ${colors.BACKGROUND_LIGHT_GRAY};
-`;
-
-const RoomInfoWrapper = styled.div`
-  position: absolute;
-  top: 50%;
-  transform: translateY(-50%);
-  display: flex;
-  align-items: center;
-  span {
-    color: ${colors.TEXT_WHITE};
-    font-weight: bold;
-    font-size: 2rem;
-    text-shadow: 1px 1px 3px ${colors.TEXT_BLACK};
-    margin-left: 1rem;
-  }
-`;
-
-const EditRoomNameImage = styled.img.attrs({
-  src:
-    'https://iconmonstr.com/wp-content/g/gd/makefg.php?i=../assets/preview/2012/png/iconmonstr-edit-4.png&r=255&g=255&b=255',
-})`
-  width: 2.5rem;
-  cursor: pointer;
-  opacity: 0.6;
-  &:hover {
-    text-decoration: underline;
-    opacity: 1;
-  }
-  &:focus {
-    outline: none;
-  }
 `;
 
 const ButtonContainer = styled.div`
@@ -48,47 +19,7 @@ const ButtonContainer = styled.div`
   transform: translateY(-50%);
 `;
 
-const NavigationBar = styled.nav`
-  display: flex;
-  background-color: ${colors.BACKGROUND_LIGHT_WHITE};
-  height: 5rem;
-  box-shadow: 0 5px 5px -4px ${colors.TEXT_GRAY};
-  justify-content: center;
-  align-items: center;
-`;
-
-const TabMenuButton = styled.div`
-  border-bottom: ${props =>
-    props.isSelected ? `3px solid ${colors.TEXT_BLACK}` : `none`};
-  background-color: ${colors.BACKGROUND_LIGHT_WHITE};
-  font-weight: bold;
-  font-size: 2rem;
-  color: ${colors.TEXT_BLACK};
-  margin-right: 1rem;
-  cursor: pointer;
-  &:hover {
-    border-bottom: 3px solid ${colors.TEXT_BLACK};
-  }
-  &:focus {
-    outline: none;
-  }
-`;
-
 function DetailRoom({ history }) {
-  const [roomName, setRoomName] = useState('방 이름');
-  const [isQuizMenuSelected, setQuizMenuState] = useState(true);
-  const [isAnalysisMenuSelected, setAnalysisMenuState] = useState(false);
-
-  function handleRoomName() {
-    /**
-     * 현재 방의 이름을 Modal input에 출력
-     * 유저가 입력한 값을 setRoomName로 설정하고 fetch
-     * 응답이 실패로 왔을 경우 상태를 이전으로 돌리고 유저에게 알림
-     * */
-    const currentRoomName = roomName;
-    setRoomName(currentRoomName);
-  }
-
   function handlePlayButton() {
     /**
      * 퀴즈를 시작할 것인지 확인하는 Modal 출력
@@ -98,52 +29,15 @@ function DetailRoom({ history }) {
     });
   }
 
-  function resetMenuState() {
-    setQuizMenuState(false);
-    setAnalysisMenuState(false);
-  }
-
-  function handleQuizMenuClick() {
-    resetMenuState();
-    setQuizMenuState(true);
-  }
-
-  function handleAnalysisMenuClick() {
-    resetMenuState();
-    setAnalysisMenuState(true);
-  }
-
   return (
     <Background>
       <Header>
-        <RoomInfoWrapper>
-          <span>{roomName}</span>
-          <EditRoomNameImage title="이름 수정하기" onClick={handleRoomName} />
-        </RoomInfoWrapper>
+        <RoomInformation />
         <ButtonContainer>
           <YellowButton onClick={handlePlayButton}>시작하기</YellowButton>
         </ButtonContainer>
       </Header>
-      <NavigationBar>
-        <TabMenuButton
-          type="button"
-          onClick={handleQuizMenuClick}
-          isSelected={isQuizMenuSelected}
-        >
-          퀴즈
-        </TabMenuButton>
-        <TabMenuButton
-          type="button"
-          onClick={handleAnalysisMenuClick}
-          isSelected={isAnalysisMenuSelected}
-        >
-          분석
-        </TabMenuButton>
-      </NavigationBar>
-      <main>
-        {isQuizMenuSelected && <>퀴즈 컴포넌트</>}
-        {isAnalysisMenuSelected && <>분석 컴포넌트</>}
-      </main>
+      <TabContents />
     </Background>
   );
 }
