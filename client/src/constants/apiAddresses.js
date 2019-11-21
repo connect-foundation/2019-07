20f6e5e9f81255e09d565da2a9eeea5c76056e79
changicho <changicho29@gmail.com>
2019-11-21 15:04:29 +0900

    Refactor(constant): 코드리뷰 결과 반영
    ?: localhost 부분도 상수로 빼서 관리함
    기존에 callbackPage로 이동할 시 localhost로 이동하는 부분을 상수로 뺌.

--- client/src/constants/apiAddresses.js
+++ client/src/constants/apiAddresses.js
@@ -2,7 +2,8 @@ const checkRoomNumber = '/room/checkRoomNumber';
 const setNickname = 'user/setNickname';
 const setJWT = 'login/setJWT';
 const callbackPageUrl = 'callback';
-const callbackPageFullUrl = `http://localhost:3000/${callbackPageUrl}`;
+const currentHost = 'http://localhost:3000/';
+const callbackPageFullUrl = `${currentHost}${callbackPageUrl}`;
 const roomListUrl = '/host/select-room';
 
 export {
