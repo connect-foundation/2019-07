1c01044d8d7840c56131ff8bb2d467aad4398a20
changicho <changicho29@gmail.com>
2019-11-21 15:01:58 +0900

    Refactor(login-express): 코드리뷰 결과 반영
    ?: 응답 형태를 convention에 맞춤.
    응답 json의 형태를 다음과 같은 형태로 제한.
    res.json({isError, isSuccess, message, data, ...})

--- server/routes/apis/login.js
+++ server/routes/apis/login.js
@@ -27,15 +27,15 @@ router.post('/setJWT', async (req, res) => {
   };
   let profileObject;
   let getNidSuccess = false;
-
+  let errorCode = 'unknown';
   await new Promise((resolve) => {
     request.get(options, async (error, response, body) => {
       if (error || response.statusCode !== 200) {
         getNidSuccess = false;
 
-        // if (response != null) {
-        //   console.log(`error code : ${response.statusCode}`);
-        // }
+        if (response != null) {
+          errorCode = `${response.statusCode}`;
+        }
 
         resolve();
         return;
@@ -51,7 +51,9 @@ router.post('/setJWT', async (req, res) => {
 
   if (!getNidSuccess) {
     res.json({
-      success: false,
+      isError: true,
+      isSuccess: false,
+      message: `네이버 프로필 API를 호출할 수 없습니다. 에러코드 : ${errorCode}`,
     });
     return;
   }
@@ -86,7 +88,7 @@ router.post('/setJWT', async (req, res) => {
 
   res.cookie('jwt', token);
   res.json({
-    success: true,
+    isSuccess: true,
   });
 });
 
