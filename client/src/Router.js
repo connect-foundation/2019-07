efa39c6accaa897c0e2b12fb12eb5a6f08cd5374
2ssue <e2ssue@gmail.com>
2019-11-21 19:06:38 +0900

    Feat: 상세 페이지 라우팅 추가
    ?: DB-백엔드 미구현으로 room/:roomId/detail이 아닌 room/detail로 라우팅

--- client/src/Router.js
+++ client/src/Router.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import MainPage from './pages/MainPage';
 import HostWaitingRoom from './pages/host/HostWaitingRoom';
-
+import HostDetailRoom from './pages/host/HostDetailRoom';
 import PlayerWaitingRoom from './pages/player/PlayerWaitingRoom';
 import CallBackPage from './pages/login/CallBackPage';
 
@@ -17,6 +17,7 @@ export default function() {
       <Route exact path="/host" component={HostWaitingRoom} />
       <Route exact path="/player" component={PlayerWaitingRoom} />
       <Route exact path="/callback" component={CallBackPage} />
+      <Route exact path="/host/room/detail" component={HostDetailRoom} />
     </Router>
   );
 }
