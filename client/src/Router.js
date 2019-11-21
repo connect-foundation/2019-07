21c38f715701f7e0208415aef22c83cadeeb57de
changicho <changicho29@gmail.com>
2019-11-21 10:36:32 +0900

    Fix(Router): 메인 페이지 라우팅 경로 오류 수정
    ?: 메인 페이지 컴포넌트는 pages 폴더 최상단에 위치.
    메인페이지 컴포넌트 경로가 잘못된 것을 수정

--- client/src/Router.js
+++ client/src/Router.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import MainPage from './pages/mainPage/MainPage';
+import MainPage from './pages/MainPage';
 import HostWaitingRoom from './pages/host/HostWaitingRoom';
 
 import PlayerWaitingRoom from './pages/player/PlayerWaitingRoom';
