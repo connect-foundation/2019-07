decace39ca5e8ad612ab30f9e0763f76fe4fa7e3
changicho <changicho29@gmail.com>
2019-11-21 13:20:06 +0900

    Refactor: JWT토큰 설정 API 경로 변경
    ?: 기존에 callback은 의미기 모호했으므로 setJWT로 변경.
    API 호출 URL 변경 및 apidoc에 맞춘 주석 추가.
    내부에 나중에 회원 정보를 저장할 때를 대비한 내용 추가.
    
    Related : 34d72556465e00cf032148e5167bbe407675689c

--- server/routes/apis/login.js
+++ server/routes/apis/login.js
@@ -6,50 +6,74 @@ require('dotenv').config();
 
 const router = express.Router();
 
-let jwtObj = {};
+const jwtObj = {};
 jwtObj.secret = 'apple';
 
-router.post('/callback', async function(req, res) {
+/**
+ * @api {post} /login/setJWT 네이버 프로필 조회 후 쿠키에 jwt로 설정하는 API.
+ * @apiName setJWT
+ * @apiGroup Login
+ *
+ * @apiParam {string} access_token 네이버 로그인 후 제공한 적근 토큰
+ */
+router.post('/setJWT', async (req, res) => {
   const accessToken = req.body.data.access_token;
-  const header = 'Bearer ' + accessToken; // Bearer 다음에 공백 추가
-  const api_url = 'https://openapi.naver.com/v1/nid/me';
+  const header = `Bearer ${accessToken}`; // Bearer 다음에 공백을 추가해야함
+  const apiUrl = 'https://openapi.naver.com/v1/nid/me';
 
   const options = {
-    url: api_url,
+    url: apiUrl,
     headers: { Authorization: header },
   };
   let profileObject;
-  let isSuccess = false;
+  let getNidSuccess = false;
 
-  await new Promise(resolve => {
+  await new Promise((resolve) => {
     request.get(options, async (error, response, body) => {
-      if (error || response.statusCode != 200) {
-        console.log('error');
-        isSuccess = false;
-        if (response != null) {
-          console.log('error code : ' + response.statusCode);
-        }
+      if (error || response.statusCode !== 200) {
+        getNidSuccess = false;
+
+        // if (response != null) {
+        //   console.log(`error code : ${response.statusCode}`);
+        // }
+
         resolve();
         return;
       }
 
-      let profile = JSON.parse(body);
+      const profile = JSON.parse(body);
 
       profileObject = profile.response;
-      isSuccess = true;
+      getNidSuccess = true;
       resolve();
     });
   });
 
-  if (!isSuccess) {
+  if (!getNidSuccess) {
     res.json({
       success: false,
     });
     return;
   }
 
-  console.log(profileObject);
-  let token = jwt.sign(
+  /**
+   * 여기서 프로필 조회 API에서 받아온 정보를 DB에 저장해야함.
+   */
+  /**
+   * 프로필객체 예시
+   * profileObject : {
+   *   "email": "openapi@naver.com",
+   *   "nickname": "OpenAPI",
+   *   "profile_image": "https://ssl.pstatic.net/static/pwe/address/nodata_33x33.gif",
+   *   "age": "40-49",
+   *   "gender": "F",
+   *   "id": "32742776",
+   *   "name": "오픈 API",
+   *   "birthday": "10-01"
+   * }
+   */
+
+  const token = jwt.sign(
     {
       email: profileObject.email,
       name: profileObject.name,
