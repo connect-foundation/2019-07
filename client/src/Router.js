f17a39ebcc3f92b0d2842b868a9ad30df31da584
leecoders <als-1994@hanmail.net>
2019-11-21 18:42:36 +0900

    Chore: 라우터 줄맞춤

--- client/src/Router.js
+++ client/src/Router.js
@@ -2,11 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import MainPage from './pages/MainPage';
 import HostWaitingRoom from './pages/host/HostWaitingRoom';
-<<<<<<< HEAD
 import EditPage from './pages/host/EditPage';
-=======
-
->>>>>>> 5abd1ddbcc3a5b58e2ccc3fa5eea0e9a7e7ef5ac
 import PlayerWaitingRoom from './pages/player/PlayerWaitingRoom';
 import CallBackPage from './pages/login/CallBackPage';
 
@@ -20,11 +16,8 @@ export default function() {
       </Switch>
       <Route exact path="/host" component={HostWaitingRoom} />
       <Route exact path="/player" component={PlayerWaitingRoom} />
-<<<<<<< HEAD
       <Route exact path="/edit" component={EditPage} />
-=======
       <Route exact path="/callback" component={CallBackPage} />
->>>>>>> 5abd1ddbcc3a5b58e2ccc3fa5eea0e9a7e7ef5ac
     </Router>
   );
 }
