c9c66b2b5e0873dcd88545907c79bc1f020fddc5
leecoders <als-1994@hanmail.net>
2019-11-21 11:32:45 +0900

    Feat: 퀴즈 편집 -> 아이템 카드 컴포넌트 생성
    ?: 퀴즈 편집 화면의 객관식 항목이 될 아이템 카드 컴포넌트 생성
    
    color string 받아서 렌더링
    체크박스, 타이틀을 state로 가짐(상위 컴포넌트로 올려야 할 필요가 있음)

---
+++ client/src/components/edit/ItemCard.js
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import checkMark from '../../assets/images/checkMark.png';
+
+const ItemCardWrapper = styled.div`
+  position: relative;
+  flex: 1 1 calc(50% - 0.5rem);
+  height: 30%;
+  margin: 0 0.25rem;
+  background-color: ${props => props.ItemCardColor};
+  border-radius: 0.4rem;
+`;
+
+const ItemInput = styled.input`
+  position: relative;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  width: 75%;
+  border: none;
+  outline: none;
+  background: none;
+  font-size: 1.5rem;
+  color: #ffffff;
+  @media (min-width: 1000px) {
+    font-size: 2rem;
+  }
+`;
+
+const ItemCheckBox = styled.div`
+  position: absolute;
+  top: 0.5rem;
+  right: 0.5rem;
+  width: 2rem;
+  height: 2rem;
+  border: 2px solid #ffffff;
+  border-radius: 50%;
+  background: ${props => props.isChecked && 'green'} url(${checkMark}) no-repeat
+    50% 50%;
+  background-size: 1rem 1rem;
+  cursor: pointer;
+  @media (min-width: 1000px) {
+    top: 50%;
+    transform: translateY(-50%);
+    right: 1rem;
+    width: 2.5rem;
+    height: 2.5rem;
+    background-size: 1.5rem 1.5rem;
+    border: 2px solid #ffffff;
+  }
+`;
+
+function ItemCard({ ItemCardColor }) {
+  const [isChecked, setChecked] = useState(false);
+  const [title, setTitle] = useState('');
+
+  function handleInputChange(e) {
+    setTitle(e.target.value);
+  }
+
+  function handleCheckBoxClick() {
+    setChecked(!isChecked);
+  }
+
+  return (
+    <ItemCardWrapper ItemCardColor={ItemCardColor}>
+      <ItemInput onChange={handleInputChange} />
+      <ItemCheckBox isChecked={isChecked} onClick={handleCheckBoxClick} />
+    </ItemCardWrapper>
+  );
+}
+
+ItemCard.propTypes = {
+  ItemCardColor: PropTypes.string.isRequired,
+};
+
+export default ItemCard;
