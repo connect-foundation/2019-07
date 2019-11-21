a89527eba132ecf31a0c1563d48a8a1b277ff5d3
changicho <changicho29@gmail.com>
2019-11-21 10:39:40 +0900

    Design(login): 로그인 페이지 디자인 테마에 맞춤.
    ?: 실제 로그인 페이지에 사용하기 위함.
    메인페이지 컴포넌트와 같은 테마를 가진다.
    내부 버튼을 네이버 아이디로 로그인 버튼으로 교체.
    로고는 표시하지 않는다.

--- client/src/pages/login/LoginPage.js
+++ client/src/pages/login/LoginPage.js
@@ -2,9 +2,37 @@ import React from 'react';
 import styled from 'styled-components';
 import NaverLogin from '../../utils/naverLoginSdk';
 import loginImage from '../../assets/images/login.PNG';
+import CopyrightFooter from '../../components/common/CopyrightFooter';
+import { PRIMARY_LIGHT_YELLOW } from '../../constants/colors';
+
+const Background = styled.div`
+  position: relative;
+  display: flex;
+  height: 100%;
+  flex-direction: column;
+  background-color: ${PRIMARY_LIGHT_YELLOW};
+  overflow-x: hidden;
+`;
+
+const ContentSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 1 1 auto;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+`;
+
+const MainContainer = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  flex: 1 1 0%;
+  padding: 5rem 0;
+  width: 100%;
+`;
 
 const ButtonNoStyle = styled.button`
-  width: 8rem;
   background: none;
   cursor: pointer;
   color: #fff;
@@ -17,19 +45,24 @@ const callbackUrl = 'http://localhost:3000/callback';
 
 function LoginPage() {
   return (
-    <div className="App">
-      <NaverLogin
-        clientId="UuzGjP3W5wERxwznlaYv"
-        callbackUrl={callbackUrl}
-        render={(props) => (
-          <ButtonNoStyle onClick={props.onClick} type="button">
-            <img src={loginImage} height="60" />
-          </ButtonNoStyle>
-        )}
-        onSuccess={(result) => console.log(result)}
-        onFailure={(result) => console.error(result)}
-      />
-    </div>
+    <Background>
+      <ContentSection>
+        <MainContainer>
+          <NaverLogin
+            clientId="UuzGjP3W5wERxwznlaYv"
+            callbackUrl={callbackUrl}
+            render={props => (
+              <ButtonNoStyle onClick={props.onClick} type="button">
+                <img src={loginImage} height="60" />
+              </ButtonNoStyle>
+            )}
+            onSuccess={result => console.log(result)}
+            onFailure={result => console.error(result)}
+          />
+        </MainContainer>
+        <CopyrightFooter />
+      </ContentSection>
+    </Background>
   );
 }
 
