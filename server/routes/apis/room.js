1b904bc7a359247c7916085465633e62051f18f5
2ssue <e2ssue@gmail.com>
2019-11-25 20:57:05 +0900

    Refactor: 방과 관련된 api post 형태에서 get 형태로 수정
    ?: 방과 닉네임 유효성 체크를 미들웨어로 바꿔서 구현함
    닉네임을 체크할 때도 방의 유효성을 체크할 필요가 있기 때문에,
    좀 더 간편한 확인을 위해 유효성 체크를 미들웨어로 변경하였음

--- server/routes/apis/room.js
+++ server/routes/apis/room.js
@@ -1,43 +1,22 @@
 const express = require('express');
-const checkJsonHasKeys = require('../../utils/checkJsonHasKeys');
-const rooms = require('../../models/rooms');
+const {
+  isRoomExist,
+  isRoomNumberValid,
+  isValidNickname,
+  isNicknameOverlap,
+} = require('../../middleware/validations');
 
 const router = express.Router();
 
 /**
- * 방 번호를 입력받으면 그 방 번호가 유효한지 확인해줌
- *
- * @param {string} roomNumber 6자리 숫자로 이루어진 방 번호
- *
- * @returns {bool} 번호가 유효한지(6자리 숫자) 아닌지 여부
- */
-function isRoomNumberValid(roomNumber) {
-  if (roomNumber.length !== 6) return false;
-  if (/[^0-9]/.test(roomNumber)) return false;
-  return true;
-}
-
-/**
- * 방 번호를 입력받으면 그 방이 열려있는지 확인해줌
- *
- * @param {string} roomNumber 6자리 숫자로 이루어진 방 번호
- *
- * @returns {bool} isExist 존재하는지 아닌지 여부
- */
-function isRoomExist(roomNumber) {
-  return !!rooms.getRoom(roomNumber);
-}
-
-/**
- * @api {post} /room/checkRoomNumber 유효한 방인지 확인 요청
- * @apiName checkRoomNumber
+ * @api {get} /room/:roomNumber 유효한 방인지 확인 요청
+ * @apiName checkValidRoomNumber
  * @apiGroup room
  *
- * @apiParam {roomNumber} id 방의 고유한 6자리 번호.
+ * @apiParam {String} roomNumber 방의 고유한 6자리 번호.
  *
- * @apiSuccess {bool} isSuccess 방을 들어갈 수 있는지 여부
- * @apiSuccess {bool} isError 정상 동작 되지 않는 이유
- * @apiSuccess {string} message 오류가 발생한 경우, 오류 메시지
+ * @apiSuccess {boolean} isSuccess 방을 들어갈 수 있는지 여부
+ * @apiSuccess {String} message 오류가 발생한 경우, 오류 메시지
  *
  * @apiSuccessExample Success-Response:
  *     HTTP/1.1 200 OK
@@ -45,54 +24,41 @@ function isRoomExist(roomNumber) {
  *       isSuccess: true,
  *     }
  *
- * @apiSuccessExample req.body에 인자 없음:
+ * @apiSuccessExample 열리지 않는 방에 접근함
  *     HTTP/1.1 200 OK
- *     {
- *       isError: true,
- *       isSuccess: false,
- *       message: 'req.body에 roomNumber가 없습니다.',
- *     }
- *
- * @apiSuccessExample 열리지 않은 방에 접근하려고 함:
- *     HTTP/1.1 200
  *     {
  *       isSuccess: false,
  *       message: '존재하지 않는 방입니다. 방 번호를 다시 입력해주세요.',
  *     }
  */
-router.post('/checkRoomNumber', (req, res) => {
-  const keys = ['roomNumber'];
-
-  if (!checkJsonHasKeys(req.body, keys)) {
-    res.json({
-      isError: true,
-      isSuccess: false,
-      message: 'req.body에 roomNumber가 없습니다.',
-    });
-    return;
-  }
-
-  // 유효한 번호인지 검사
-  if (!isRoomNumberValid(req.body.roomNumber)) {
-    res.json({
-      isSuccess: false,
-      message: '유효하지 않은 방입니다. 방 번호를 다시 입력해주세요.',
-    });
-    return;
-  }
-
-  // 유효한 방인지 검사
-  if (!isRoomExist(req.body.roomNumber)) {
-    res.json({
-      isSuccess: false,
-      message: '존재하지 않는 방입니다. 방 번호를 다시 입력해주세요.',
-    });
-    return;
-  }
-
+router.get('/:roomNumber', isRoomNumberValid, isRoomExist, (req, res) => {
   res.json({
     isSuccess: true,
   });
 });
+
+/**
+ * @api {get} /room/:roomNumber/:nickname 방에서 유효한 닉네임인지 확인 요청
+ * @apiName checkValidNickname
+ * @apiGroup room
+ *
+ * @apiParam {String} roomNumber 방의 고유한 6자리 번호.
+ * @apiParam {String} nickname 유저가 입력한 닉네임.
+ *
+ * @apiSuccess {boolean} isSuccess 입력한 닉네임으로 방을 들어갈 수 있는지 여부
+ * @apiSuccess {String} message 오류가 발생한 경우, 오류 메시지
+ */
+router.get(
+  '/:roomNumber/:nickname',
+  isRoomNumberValid,
+  isRoomExist,
+  isValidNickname,
+  isNicknameOverlap,
+  (req, res) => {
+    res.json({
+      isSuccess: true,
+    });
+  },
+);
 
 module.exports = router;
