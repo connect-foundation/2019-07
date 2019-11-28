67a16c39b5c2c80d623b2777566220cf69b857d4
changicho <changicho29@gmail.com>
2019-11-28 15:55:31 +0900

    Feat: 퀴즈 진행에 사용하는 API들 추가.
    ?: 소켓으로 데이터들을 전송하는 데 문제가 있어 API로 제작.
    1. 현재 진행할 퀴즈 세트의 정보를 가져오는 API.
    2. 지금까지 유저 점수의 총 합을 알려주는 API.
    3. 퀴즈가 종료된 후 유저의 결과 (점수, 등수)를 알려주는 API.
    총 3개의 API 추가.
    DB manager 객체가 필요한 부분은 주석으로 남겨놓음.
    방에 유저가 존재하는지 검사하는 middleware 추가.
    (express-validator)이용.

--- server/routes/api.js
+++ server/routes/api.js
@@ -4,8 +4,10 @@ const router = express.Router();
 
 const room = require('./apis/room');
 const login = require('./apis/login');
+const game = require('./apis/game');
 
 router.use('/room', room);
 router.use('/login', login);
+router.use('/', game);
 
 module.exports = router;
