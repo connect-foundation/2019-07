273103e62eb0a1e2bf4de6a818ed89e9ac7e49db
2ssue <e2ssue@gmail.com>
2019-11-28 18:34:32 +0900

    Rename: WaitingRoom 파일 이름 변경에 따른 Routing 설정 변경
    ?: 페이지 이동에 소켓 이슈가 있어 WaitingRoom에서 GameRoom으로 변경

--- client/src/Router.js
+++ client/src/Router.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import MainPage from './pages/MainPage';
-import HostWaitingRoom from './pages/host/HostWaitingRoom';
+import HostGameRoom from './pages/host/HostGameRoom';
 import HostDetailRoom from './pages/host/HostDetailRoom';
 import EditPage from './pages/host/EditPage';
 import PlayerWaitingRoom from './pages/player/PlayerWaitingRoom';
@@ -15,7 +15,7 @@ export default function() {
         <Route path="/nickname" component={MainPage} />
         <Route path="/login" component={MainPage} />
       </Switch>
-      <Route exact path="/host" component={HostWaitingRoom} />
+      <Route exact path="/host" component={HostGameRoom} />
       <Route exact path="/player" component={PlayerWaitingRoom} />
       <Route exact path="/edit" component={EditPage} />
       <Route exact path="/callback" component={CallBackPage} />
