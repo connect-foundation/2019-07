991308fd697de54cf61278477a1ffa26ec5a3663
changicho <changicho29@gmail.com>
2019-12-03 13:47:44 +0900

    Rename: Quizset 클래스 파일 생성
    ?: git에서 대소문자 구별을 하지 않기 때문에 삭제하고 다시 생성.
    
    Related : e15b42347af049f3cbcb5a4593c5c06e54cc664d

---
+++ server/models/database/tables/Quizset.js
@@ -0,0 +1,13 @@
+const Table = require('./Table');
+
+class Quizset extends Table {
+  /**
+   * 메소드 가이드라인
+   *
+   * method() {
+   *   return this.query(query, params);
+   * }
+   */
+}
+
+module.exports = Quizset;
