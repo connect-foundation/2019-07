f335e894c7c9288a5219e3e64233ac4968ac34aa
2ssue <e2ssue@gmail.com>
2019-12-03 21:17:20 +0900

    Feat: Modal을 사용하기 위해 최상위에 Modal Provider 사용
    ?: Toast와 마찬가지로, Modal 또한 최상위에 Provider가 있어야함

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Router from './Router';
 import ToastProvider from './components/common/ToastProvider';
+import ModalProvider from './components/common/ModalProvider';
 
 function App() {
   return (
     <ToastProvider>
-      <Router />
+      <ModalProvider>
+        <Router />
+      </ModalProvider>
     </ToastProvider>
   );
 }
