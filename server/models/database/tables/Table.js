84cb01cc2197f5a2179edde5f92dee40bf1b9c2c
changicho <changicho29@gmail.com>
2019-12-03 11:23:26 +0900

    Feat(dbManager): 트랜잭션 메소드 추가.
    ?: 트랜잭션을 사용할 경우를 위해 트랜잭션 메소드 추가.
    query 메소드 실행 시, connection에서 오류가 생기는 경우를 방지하기 위해
    try catch로 한번 감싸서 처리.
    트랜잭션용 메소드를 transaction으로 추가함.

--- server/models/database/tables/Table.js
+++ server/models/database/tables/Table.js
@@ -11,39 +11,80 @@ class Table {
   }
 
   /**
-   * connection pool에서 연결을 받아오는 함수
-   *
-   * @returns {object} mysql2에서 연결 후 넘긴 connection 객체
-   * @memberof Table
-   */
-  async connection() {
-    return this.pool.getConnection(async (conn) => conn);
-  }
-
-  /**
    * connection 후 쿼리를 수행해주는 함수
    * 풀을 받아옴(연결) - 쿼리 - 풀 반납 순서를 거친다.
    *
    * @param {string} query mysql에서 실행할 쿼리
    * @param {list} args 쿼리의 ?에 들어갈 인자
-   * @returns {object} rows 쿼리 후 받아온 결과 (성공시)
+   * @returns {object} rows 쿼리 후 받아온 rows를 포함한 객체 (성공시)
    * @returns {object} 에러 발생 시 return하는 객체 (에러)
    * @memberof Table
    */
   async query(query, ...args) {
-    const connection = await this.connection();
-
     try {
-      const [rows] = await connection.query(query, args);
+      const connection = await this.pool.getConnection(async (conn) => conn);
 
-      return JSON.parse(JSON.stringify(rows));
-    } catch (err) {
+      try {
+        const [rows] = await connection.query(query, args);
+
+        return {
+          isSuccess: true,
+          data: JSON.parse(JSON.stringify(rows)),
+        };
+      } catch (error) {
+        return {
+          isError: true,
+          message: error.message,
+        };
+      } finally {
+        connection.release();
+      }
+    } catch (error) {
       return {
-        success: false,
-        message: err.message,
+        isError: true,
+        message: error.message,
       };
-    } finally {
-      connection.release();
+    }
+  }
+
+  /**
+   * connection 후 트랜잭션을 수행해주는 함수
+   * 풀을 받아옴(연결) - 트랜잭션 시작 - 쿼리 - 커밋/롤백 - 풀 반납 순서를 거친다.
+   *
+   * @param {string} query mysql에서 실행할 쿼리
+   * @param {list} args 쿼리의 ?에 들어갈 인자
+   * @returns {object} rows 쿼리 후 받아온 rows를 포함한 객체 (성공시)
+   * @returns {object} 에러 발생 시 return하는 객체 (에러)
+   * @memberof Table
+   */
+  async transaction(query, ...args) {
+    try {
+      const connection = await this.pool.getConnection(async (conn) => conn);
+
+      try {
+        await connection.beginTransaction();
+        const [rows] = await connection.query(query, args);
+        await connection.commit();
+
+        return {
+          isSuccess: true,
+          data: JSON.parse(JSON.stringify(rows)),
+        };
+      } catch (error) {
+        await connection.rollback();
+
+        return {
+          isError: true,
+          message: error.message,
+        };
+      } finally {
+        connection.release();
+      }
+    } catch (error) {
+      return {
+        isError: true,
+        message: error.message,
+      };
     }
   }
 }
