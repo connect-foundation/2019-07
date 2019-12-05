13cb9e275d55a26a39183a92ed8744679c069bf5
changicho <changicho29@gmail.com>
2019-12-05 16:23:53 +0900

    Refactor: 메소드가 추가된 manager 내부 객체의 요소 삭제
    ?: 필요 없는 주석을 삭제하기위해.
    앞으로 최초의 메소드를 추가하는 사람이 메소드 가이드라인 주석을 지우도록
    한다.

--- server/models/database/tables/User.js
+++ server/models/database/tables/User.js
@@ -2,14 +2,6 @@ const Table = require('./Table');
 const { userTable } = require('../../../constants/tableName');
 
 class User extends Table {
-  /**
-   * 메소드 가이드라인
-   *
-   * method() {
-   *   return this.query(query, params);
-   * }
-   */
-
   insertUser({ email }) {
     return this.query(`INSERT INTO ${userTable} (email) VALUES ('${email}')`);
   }
