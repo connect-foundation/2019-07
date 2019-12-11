7fd0292769130ee0aba4f482469d7e7d92bed9aa
Changyeong <ocy1011@naver.com>
2019-12-11 14:33:11 +0900

    Feat: edit api 추가
     ?: 퀴즈 편집 페이지에서 사용하는 edit api 추가

--- server/routes/api.js
+++ server/routes/api.js
@@ -6,10 +6,12 @@ const room = require('./apis/room');
 const login = require('./apis/login');
 const game = require('./apis/game');
 const user = require('./apis/user');
+const edit = require('./apis/edit');
 
 router.use('/room', room);
 router.use('/login', login);
 router.use('/user', user);
+router.use('/edit', edit);
 router.use('/', game);
 
 module.exports = router;
