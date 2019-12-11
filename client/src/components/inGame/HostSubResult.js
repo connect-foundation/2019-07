8e668c4e223ba0d03011b5a080e16e3d6a918bd2
2ssue <e2ssue@gmail.com>
2019-12-11 13:44:39 +0900

    Feat: 맨 마지막 퀴즈에서 '최종결과'버튼이 출력되도록 변경
    ?: 렌더링 시 확인을 거쳐 버튼 이름을 바꾸도록 설정함
    rendering되는 컴포넌트들을 한눈에 보기 위해 onClick을 외부 함수로
    따로 작성해 리팩토링 하였음.

--- client/src/components/inGame/HostSubResult.js
+++ client/src/components/inGame/HostSubResult.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { GreenButton } from '../common/Buttons';
 import ScoreChart from '../common/ScoreChart';
 import * as layout from './Layout';
@@ -6,6 +6,8 @@ import { HostGameAction, HostGameContext } from '../../reducer/hostGameReducer';
 
 function HostSubResult() {
   const { dispatcher, roomState } = useContext(HostGameContext);
+  const [nextButtonName, setNextButtonName] = useState('다음퀴즈');
+
   const itemDatas = roomState.quizSubResult.map((cur, index) => {
     if (roomState.currentQuiz.answers.includes(index)) {
       return { ...cur, isAnswer: true };
@@ -14,19 +16,25 @@ function HostSubResult() {
     return { ...cur, isAnswer: false };
   });
 
+  useEffect(() => {
+    if (roomState.currentQuiz.index === roomState.totalQuizCount - 1) {
+      setNextButtonName('최종결과');
+    }
+  }, [nextButtonName]);
+
+  function handleNextButtonClick() {
+    if (roomState.currentQuiz.index === roomState.totalQuizCount - 1) {
+      dispatcher({ type: HostGameAction.REQUEST_QUIZ_END });
+      return;
+    }
+    dispatcher({ type: HostGameAction.REQUEST_NEXT_QUIZ });
+  }
+
   return (
     <layout.CenterContentContainer>
       <layout.NextButtonWrapper>
-        <GreenButton
-          onClick={() => {
-            if (roomState.currentQuiz.index === roomState.totalQuizCount - 1) {
-              dispatcher({ type: HostGameAction.REQUEST_QUIZ_END });
-              return;
-            }
-            dispatcher({ type: HostGameAction.REQUEST_NEXT_QUIZ });
-          }}
-        >
-          다음퀴즈
+        <GreenButton onClick={handleNextButtonClick}>
+          {nextButtonName}
         </GreenButton>
       </layout.NextButtonWrapper>
       <layout.CenterLeftPanel />
