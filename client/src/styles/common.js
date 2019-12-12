83c8d3f19fca9db1e898bf1ba19b2578270ea5c0
changicho <changicho29@gmail.com>
2019-12-12 15:00:49 +0900

    Design: footer 모바일 페이지 사이즈 변경
    ?: 모바일 페이지의 경우 너무 작게 나오던 오류 해결
    @media를 이용해 모바일 페이지와 데스크톱 페이지에서 다른 height를 가지도
    록 설정.
    글씨 크기 또한 비례해 증가하도록 설정

--- client/src/styles/common.js
+++ client/src/styles/common.js
@@ -1,23 +1,32 @@
 import { css } from 'styled-components';
 import * as colors from '../constants/colors';
+import DESKTOP_MIN_WIDTH from '../constants/media';
 
 const InGameFooterStyle = css`
   position: relative;
   flex: none;
   justify-self: flex-end;
-  height: 5vmin;
+  height: 10vmin;
   border-top: 1px solid ${colors.BORDER_LIGHT_GRAY};
   box-sizing: border-box;
   background-color: ${colors.BACKGROUND_LIGHT_WHITE};
+
+  @media (min-width: ${DESKTOP_MIN_WIDTH}) {
+    height: 5vmin;
+  }
 `;
 
 const InGameFooterTextStyle = css`
   position: absolute;
-  font-size: 2.5vmin;
+  font-size: 5vmin;
   font-weight: bold;
   top: 50%;
   transform: translateY(-50%);
   vertical-align: middle;
+
+  @media (min-width: ${DESKTOP_MIN_WIDTH}) {
+    font-size: 2.5vmin;
+  }
 `;
 
 const InputStyle = css`
