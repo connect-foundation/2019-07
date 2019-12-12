5c20f9b2e002bc49aed37e5a64001fa67021594e
Changyeong <ocy1011@naver.com>
2019-12-12 11:53:31 +0900

    Feat: editReducer 구조 변경 및 새로운 기능 추가
     ?: item과 quiz의 템플릿을 분리하여 보기 좋게 수정함
     CRUD에 맞춰 action의 CHANGE를 UPDATE로 변경
     quizsetState에 읽어온 퀴즈들, 삭제된 퀴즈들 등 편집에 필요한 state 추가
     새로운 action들 추가

--- client/src/reducer/hostEditReducer.js
+++ client/src/reducer/hostEditReducer.js
@@ -1,54 +1,57 @@
-function createQuizTemplete() {
+import { parseCamelObject } from '../utils/caseChanger';
+
+const createItem = itemOrder => {
   return {
-    quizId: -1,
+    id: undefined,
+    itemOrder,
     title: '',
-    image: '',
-    items: [
-      {
-        title: '',
-        isAnswer: false,
-      },
-      {
-        title: '',
-        isAnswer: false,
-      },
-      {
-        title: '',
-        isAnswer: false,
-      },
-      {
-        title: '',
-        isAnswer: false,
-      },
-    ],
+    isAnswer: false,
+  };
+};
+const createQuiz = () => {
+  return {
+    id: undefined,
+    title: '',
+    quizOrder: 0,
+    imagePath: null,
+    items: [createItem(0), createItem(1), createItem(2), createItem(3)],
     timeLimit: 30,
     score: 1000,
   };
-}
+};
 
 const initialQuizsetState = {
-  quizsetId: -1,
-  quizset: [createQuizTemplete()],
+  roomId: undefined,
+  quizsetId: undefined,
+  quizset: [],
+  readedQuizset: [],
+  deletedQuizzes: [],
+  deleteCount: 0,
   currentIndex: 0,
   isTimeLimitOpend: false,
+  isLoading: true,
 };
 
 const actionTypes = {
-  ADD_QUIZ: 0,
-  CHANGE_TITLE: 1,
-  CHANGE_IMAGE: 2,
-  CHANGE_SCORE: 3,
-  CHANGE_TIME_LIMIT: 4,
-  CHANGE_ITEM_IS_ANSWER: 5,
-  CHANGE_ITEM_TITLE: 6,
-  CHNAGE_CURRENT_INDEX: 7,
+  CREATE_QUIZ: 0,
+  UPDATE_TITLE: 1,
+  UPDATE_IMAGE: 2,
+  UPDATE_SCORE: 3,
+  UPDATE_TIME_LIMIT: 4,
+  UPDATE_ITEM_IS_ANSWER: 5,
+  UPDATE_ITEM_TITLE: 6,
+  UPDATE_CURRENT_INDEX: 7,
   ON_OFF_TIME_LIMIT: 8,
   READ_QUIZSET: 9,
+  DELETE_QUIZ: 10,
+  UPDATE_ROOM_ID: 11,
+  CHANGE_LOADING: 12,
 };
 
 function addQuiz(quizset) {
-  return [...quizset, createQuizTemplete()];
+  return [...quizset, createQuiz()];
 }
+
 function updateArray(array, index, element) {
   const newArray = array.slice();
   newArray.splice(index, 1, element);
@@ -57,44 +60,45 @@ function updateArray(array, index, element) {
 
 const quizsetReducer = (quizsetState, action) => {
   const { quizset, currentIndex } = quizsetState;
-  const quiz = currentIndex === -1 ? undefined : quizset[currentIndex];
+  const quiz = quizset[currentIndex];
   switch (action.type) {
-    case actionTypes.ADD_QUIZ: {
+    case actionTypes.CREATE_QUIZ: {
       return {
         ...quizsetState,
         quizset: addQuiz(quizset),
         currentIndex: quizset.length,
         isTimeLimitOpend: false,
+        isLoading: false,
       };
     }
-    case actionTypes.CHNAGE_CURRENT_INDEX: {
+    case actionTypes.UPDATE_CURRENT_INDEX: {
       return {
         ...quizsetState,
         currentIndex: action.currentIndex,
         isTimeLimitOpend: false,
       };
     }
-    case actionTypes.CHANGE_TITLE: {
+    case actionTypes.UPDATE_TITLE: {
       quiz.title = action.title;
       const newQuizset = updateArray(quizset, currentIndex, quiz);
       return { ...quizsetState, quizset: newQuizset };
     }
-    case actionTypes.CHANGE_IMAGE: {
-      quiz.image = action.image;
+    case actionTypes.UPDATE_IMAGE: {
+      quiz.imagePath = action.imagePath;
       const newQuizset = updateArray(quizset, currentIndex, quiz);
       return { ...quizsetState, quizset: newQuizset };
     }
-    case actionTypes.CHANGE_SCORE: {
+    case actionTypes.UPDATE_SCORE: {
       quiz.score = action.score;
       const newQuizset = updateArray(quizset, currentIndex, quiz);
       return { ...quizsetState, quizset: newQuizset };
     }
-    case actionTypes.CHANGE_TIME_LIMIT: {
+    case actionTypes.UPDATE_TIME_LIMIT: {
       quiz.timeLimit = action.timeLimit;
       const newQuizset = updateArray(quizset, currentIndex, quiz);
       return { ...quizsetState, quizset: newQuizset, isTimeLimitOpend: false };
     }
-    case actionTypes.CHANGE_ITEM_IS_ANSWER: {
+    case actionTypes.UPDATE_ITEM_IS_ANSWER: {
       const { itemIndex, itemIsAnswer } = action;
       const { items } = quiz;
       const item = items[itemIndex];
@@ -103,7 +107,7 @@ const quizsetReducer = (quizsetState, action) => {
       const newQuizset = updateArray(quizset, currentIndex, quiz);
       return { ...quizsetState, quizset: newQuizset };
     }
-    case actionTypes.CHANGE_ITEM_TITLE: {
+    case actionTypes.UPDATE_ITEM_TITLE: {
       const { itemIndex, itemTitle } = action;
       const { items } = quiz;
       const item = items[itemIndex];
@@ -119,7 +123,34 @@ const quizsetReducer = (quizsetState, action) => {
       };
     }
     case actionTypes.READ_QUIZSET: {
-      return { ...quizsetState, quizset: action.newQuizset };
+      const getNewQuizset = () => parseCamelObject(action.quizset);
+      return {
+        ...quizsetState,
+        quizset: getNewQuizset(),
+        readedQuizset: getNewQuizset(),
+        isLoading: false,
+      };
+    }
+    case actionTypes.DELETE_QUIZ: {
+      const { deletedQuizzes, deleteCount } = quizsetState;
+      const [deletedQuiz] = quizset.splice(currentIndex, 1);
+      if (deletedQuiz.id !== undefined) deletedQuizzes.push(deletedQuiz);
+      const nextIndex = Math.min(currentIndex, Math.max(quizset.length - 1, 0));
+      const nextQuizset = quizset.length > 0 ? quizset : [createQuiz()];
+      return {
+        ...quizsetState,
+        deletedQuizzes,
+        quizset: nextQuizset,
+        currentIndex: nextIndex,
+        isTimeLimitOpend: false,
+        deleteCount: deleteCount + 1,
+      };
+    }
+    case actionTypes.UPDATE_ROOM_ID: {
+      return { ...quizsetState, roomId: action.roomId };
+    }
+    case actionTypes.CHANGE_LOADING: {
+      return { ...quizsetState, isLoading: action.isLoading };
     }
     default:
       return quizsetState;
