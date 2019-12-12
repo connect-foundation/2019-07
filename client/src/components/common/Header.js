d7f5e35ea81f026a51673dbe65c5d831628a74e8
2ssue <e2ssue@gmail.com>
2019-12-12 18:31:34 +0900

    Design: 헤더 높이 렌더링 문제 수정
    ?: Edit 페이지와 똑같게 렌더링 되던 헤더 높이를 수정함

--- client/src/components/common/Header.js
+++ client/src/components/common/Header.js
@@ -5,23 +5,16 @@ import PropTypes from 'prop-types';
 import * as colors from '../../constants/colors';
 import DESKTOP_MIN_WIDTH from '../../constants/media';
 
-const HeaderHeight = '8vmin';
-
 const HeaderArea = styled.div.attrs({
   className: 'headerArea',
 })`
   position: relative;
-  width: 100%;
-  height: ${HeaderHeight};
-  flex: none;
-`;
-
-const HeaderStyle = styled.header`
-  position: fixed;
-  width: 100%;
-  height: ${HeaderHeight};
   box-shadow: 5px 5px 5px ${colors.BACKGROUND_DEEP_GRAY};
   background-color: ${colors.PRIMARY_DEEP_GREEN};
+  padding: 1rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   z-index: 900;
 `;
 
@@ -31,23 +24,17 @@ const ServiceLogoImage = styled.img.attrs({
   @media (min-width: ${DESKTOP_MIN_WIDTH}) {
     width: 16rem;
   }
-  position: absolute;
-  left: 50%;
   width: 10rem;
-  transform: translateX(-50%) translateY(-50%);
-  top: 50%;
   cursor: pointer;
 `;
 
 function Header({ children }) {
   return (
     <HeaderArea>
-      <HeaderStyle>
-        <Link to="/">
-          <ServiceLogoImage />
-        </Link>
-        {children}
-      </HeaderStyle>
+      <Link to="/">
+        <ServiceLogoImage />
+      </Link>
+      {children}
     </HeaderArea>
   );
 }
