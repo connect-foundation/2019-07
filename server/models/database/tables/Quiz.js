e97cebb2dc2656cefacc6f6522fc6e0af865e191
Changyeong <ocy1011@naver.com>
2019-12-13 12:36:56 +0900

    Feat: Quiz 데이터 관리 구조 변경
     ?: 이미지를 업로드 하는 기능이 추가되면서 구조를 변경함

--- server/models/database/tables/Quiz.js
+++ server/models/database/tables/Quiz.js
@@ -3,28 +3,20 @@ const { quizTable } = require('../../../constants/tableName');
 
 class Quiz extends Table {
   createQuiz(quizsetId, quiz) {
-    const { title, quizOrder, score, timeLimit, imagePath } = quiz;
-    const insert = `INSERT INTO ${quizTable} (title, quiz_order, score, time_limit, image_path, quizset_id)`;
-    const values = `VALUES (?, ?, ?, ?, ?, ?)`;
+    const { title, quizOrder, score, timeLimit } = quiz;
+    const insert = `INSERT INTO ${quizTable} (title, quiz_order, score, time_limit, quizset_id)`;
+    const values = `VALUES (?, ?, ?, ?, ?)`;
     const query = `${insert} ${values};`;
-    return this.query(
-      query,
-      title,
-      quizOrder,
-      score,
-      timeLimit,
-      imagePath,
-      quizsetId,
-    );
+    return this.query(query, title, quizOrder, score, timeLimit, quizsetId);
   }
 
   updateQuiz(quiz) {
-    const { id, title, quizOrder, score, timeLimit, imagePath } = quiz;
+    const { id, title, quizOrder, score, timeLimit } = quiz;
     const update = `UPDATE ${quizTable}`;
-    const set = `SET title = ?, quiz_order = ?, score = ?, time_limit = ?, image_path = ?`;
+    const set = `SET title = ?, quiz_order = ?, score = ?, time_limit = ?`;
     const where = `WHERE id = ?`;
     const query = `${update} ${set} ${where};`;
-    return this.query(query, title, quizOrder, score, timeLimit, imagePath, id);
+    return this.query(query, title, quizOrder, score, timeLimit, id);
   }
 
   readQuizzes(quizsetId) {
@@ -38,6 +30,14 @@ class Quiz extends Table {
   deleteQuiz(quizId) {
     const query = `DELETE FROM ${quizTable} WHERE id = ?;`;
     return this.query(query, quizId);
+  }
+
+  updateImagePath(quizId, imagePath) {
+    const update = `UPDATE ${quizTable}`;
+    const set = `SET image_path = ?`;
+    const where = `WHERE id = ?`;
+    const query = `${update} ${set} ${where};`;
+    return this.query(query, imagePath, quizId);
   }
 }
 
