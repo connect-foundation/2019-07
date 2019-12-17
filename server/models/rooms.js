9b2e5af42ef9f1586e88903a7a1bae75131e7e2b
2ssue <e2ssue@gmail.com>
2019-12-17 18:00:43 +0900

    Refactor: itrator 구조분해 에러 방지를 위해 try-catch문 추가
    ?: 혹시 모를 상황을 대비해 서버가 죽지 않도록 try-catch로 에러처리

--- server/models/rooms.js
+++ server/models/rooms.js
@@ -61,8 +61,8 @@ class Rooms {
     const previousArray = [];
     let quizIndex = -1;
 
-    data.forEach((currentValue) => {
-      if (!previousArray.find((element) => element === currentValue.id)) {
+    data.forEach(currentValue => {
+      if (!previousArray.find(element => element === currentValue.id)) {
         const currentQuiz = quizTemplate();
         currentQuiz.title = currentValue.quizTitle;
         currentQuiz.score = currentValue.score;
@@ -140,16 +140,20 @@ class Rooms {
     const roomList = this.rooms.entries();
 
     const findHostRoom = () => {
-      if (!this.rooms.size) return false;
       const { done, value } = roomList.next();
-      const [roomNumber, roomInformation] = value;
+      try {
+        const [roomNumber, roomInformation] = value;
 
-      if (hostId === roomInformation.hostId) return roomNumber;
-      if (done) return false;
+        if (hostId === roomInformation.hostId) return roomNumber;
+        if (done) return false;
 
-      return findHostRoom();
+        return findHostRoom();
+      } catch (err) {
+        return false;
+      }
     };
 
+    if (!this.rooms.size) return false;
     const roomNumber = findHostRoom();
 
     if (roomNumber) {
